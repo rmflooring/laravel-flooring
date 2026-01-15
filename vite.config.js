a83af61752427ea6c3858473ab538544d4aa13de
Richard Bruni <richardbruni@Richards-MacBook-Pro.local>
2026-01-15 14:09:58 -0800

    Add calendar.js to Vite build inputs

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
 
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/pages/calendar.js'],
             refresh: true,
         }),
     ],
