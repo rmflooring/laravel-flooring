a9d5d89e5e26e775cfaa786fc74e01d72f236e93
Richard Bruni <richard@rmflooring.ca>
2026-01-31 15:06:19 -0800

    Allow sidebar flyouts to overflow container

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -19,7 +19,7 @@
     <aside id="app-sidebar"
            class="fixed left-0 top-0 z-40 h-screen translate-x-0 border-r border-gray-200 bg-white transition-transform transition-all duration-200 sm:translate-x-0 dark:border-gray-700 dark:bg-gray-900"
            aria-label="Sidebar">
-        <div class="relative h-full px-3 py-4">
+        <div class="relative h-full px-3 py-4 overflow-visible">
 
             <!-- Logo -->
             <a href="{{ route('dashboard') }}" class="mb-6 flex items-center space-x-3 rtl:space-x-reverse">
